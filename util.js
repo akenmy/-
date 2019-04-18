31ef1614f0dd3b315246100c9f3fa8161222d841
akenmy <1247861388@qq.com>
2019-04-18 13:36:09 +0800

    update util.js

--- util.js
+++ util.js
@@ -198,6 +198,283 @@ const moneyFormat = (value) => {
   var num1 = numsArr[1].substring(0,2);
 return  numsArr[0] + "." +  num1
 };
+
+/**
+ * @description 数组顺序打乱
+ * @param arr
+ * @returns {*}
+ * upsetArr([1,2,3,4,5])
+ */
+const upsetArr = arr =>{
+  let j,_item;
+  for(let i=0;i<arr.length;i++){
+    j = Math.floor(Math.random() * i);
+    _item = arr[i];
+    arr[i] = arr[j];
+    arr[j] = _item;
+  }
+  return arr;
+};
+const upsetArrSimple = arr =>{
+  return arr.sort(()=>{
+    return Math.random() - 0.5;
+  })
+};
+
+/**
+ * 检测密码强度
+ * @param str
+ * @returns {number}
+ */
+const checkPwdLevelSimple = str =>{
+  let nowLv = 0;
+  if (str.length < 6)
+    return nowLv;
+  if (/[0-9]/.test(str))
+    nowLv++;
+  if (/[a-z]/.test(str))
+    nowLv++;
+  if (/[A-Z]/.test(str))
+    nowLv++;
+  if (/[\.|-|_]/.test(str))
+    nowLv++;
+  return nowLv;
+};
+const checkPwdLevel = str => {
+  let nowLv = 0;
+  if (str.length < 6)
+    return nowLv;
+  let rules=[/[0-9]/,/[a-z]/,/[A-Z]/,/[\.|-|_]/];
+  for(let i=0;i<rules.length;i++){
+    if(rules[i].test(str))
+      nowLv++;
+  }
+  return nowLv;
+};
+/**
+ * @description 设置cookie
+ * @param name cookie名称
+ * @param value 值
+ * @param iDay 有效时间（天数）
+ *
+ * ecDo.setCookie(cookieName,'守候',1)//设置（有效时间为1天）
+ * ecDo.getCookie(cookieName)//获取
+ * ecDo.removeCookie(cookieName)//删除
+ */
+const setCookie = (name, value, iDay) => {
+  let oDate = new Date();
+  oDate.setDate(oDate.getDate() + iDay);
+  document.cookie = name + '=' + value + ';expires=' + oDate;
+};
+/**
+ * @description 获取cookie
+ * @param name cookie名称
+ */
+const getCookie = name => {
+  let arr = document.cookie.split('; '),arr2;
+  for (let i = 0; i < arr.length; i++) {
+    arr2 = arr[i].split('=');
+    if (arr2[0] == name) {
+      return arr2[1];
+    }
+  }
+  return '';
+};
+/**
+ * @description 删除cookie
+ * @param name cookie名称
+ */
+const removeCookie = name=> {
+  this.setCookie(name, 1, -1);
+};
+
+/**
+ * @description 降序返回数组（字符串）每个元素的出现次数
+ * @param arr
+ * @return {Array}
+ */
+const getCount = arr => {
+  let obj = {}, k, arr1 = [];
+  //记录每一元素出现的次数
+  for (let i = 0, len = arr.length; i < len; i++) {
+    k = arr[i];
+    if (obj[k]) {
+      obj[k]++;
+    } else {
+      obj[k] = 1;
+    }
+  }
+  //保存结果{el-'元素'，count-出现次数}
+  for (let o in obj) {
+    arr1.push({el: o, count: obj[o]});
+  }
+  //排序（降序）
+  arr1.sort(function (n1, n2) {
+    return n2.count - n1.count
+  });
+  return arr1;
+};
+/**
+ * @description 检测字符串
+ */
+
+const checkType = (function(){
+  let rules={
+    email(str){       return /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(str);    },
+    mobile(str){      return /^1[3|4|5|7|8][0-9]{9}$/.test(str);    },
+    tel(str){         return /^(0\d{2,3}-\d{7,8})(-\d{1,4})?$/.test(str);    },
+    number(str){      return /^[0-9]$/.test(str);    },
+    english(str){     return /^[a-zA-Z]+$/.test(str);    },
+    text(str){        return /^\w+$/.test(str);    },
+    chinese(str){     return /^[\u4E00-\u9FA5]+$/.test(str);    },
+    lower(str){       return /^[a-z]+$/.test(str);    },
+    upper(str){       return /^[A-Z]+$/.test(str);    }
+  };
+  return {
+    /**
+     * @description 检测接口
+     * @param str 待处理字符串
+     * @param type 待检测的类型
+     */
+    check(str, type){      return rules[type]?rules[type](str):false;    },
+    /**
+     * @description 添加规则扩展接口
+     * @param type 规则名称
+     * @param fn 处理函数
+     */
+    addRule(type,fn){      rules[type]=fn;    }
+  }
+})();
+/**
+ * @description 清除左右空格
+ */
+const trim = str => {  return str.replace(/(^\s*)|(\s*$)/g, "");};
+/**
+ * @description 清除所有空格
+ */
+const trimAll = str => {  return str.replace(/\s+/g, "");};
+/**
+ * @description 清除左空格
+ */
+const trimLeft = str=> {  return str.replace(/(^\s*)/g, "");};
+/**
+ * @description 清除右空格
+ */
+const trimRight = str => {  return str.replace(/(\s*$)/g, "");};
+
+
+/**
+ * @description 加密字符串
+ * @param regIndex 加密位置  （开始加密的索引，结束加密的索引）
+ * @param ARepText 加密的字符 （默认*）
+ */
+const encryptStr = (str, regIndex, ARepText = '*') =>{
+  let regtext = '',
+    Reg = null,
+    _regIndex=regIndex.split(','),
+    replaceText = ARepText;
+  //repeatStr是在上面定义过的（字符串循环复制），大家注意哦
+  _regIndex=_regIndex.map(item=>+item);
+  regtext = '(\\w{' + _regIndex[0] + '})\\w{' + (1+_regIndex[1]-_regIndex[0]) + '}';
+  Reg = new RegExp(regtext);
+  let replaceCount = this.repeatStr(replaceText, (1+_regIndex[1]-_regIndex[0]));
+  return str.replace(Reg, '$1' + replaceCount);
+};
+/**
+ * @description 不加密字符串
+ * @param regIndex 不加密位置  （开始加密的索引，结束加密的索引）
+ * @param ARepText 不加密的字符 （默认*）
+ */
+const encryptUnStr = (str, regIndex, ARepText = '*') => {
+  let regtext = '',
+    Reg = null,
+    _regIndex=regIndex.split(','),
+    replaceText = ARepText;
+  _regIndex=_regIndex.map(item=>+item);
+  regtext = '(\\w{' + _regIndex[0] + '})(\\w{' + (1+_regIndex[1]-_regIndex[0]) + '})(\\w{' + (str.length-_regIndex[1]-1) + '})';
+  Reg = new RegExp(regtext);
+  let replaceCount1 = this.repeatStr(replaceText, _regIndex[0]);
+  let replaceCount2 = this.repeatStr(replaceText, str.length-_regIndex[1]-1);
+  return str.replace(Reg, replaceCount1 + '$2' + replaceCount2);
+};
+/**
+ * @description 字符串开始位置加密
+ * @param regIndex 加密长度
+ * @param ARepText 加密的字符 （默认*）
+ */
+const encryptStartStr = (str,length,replaceText = '*') => {
+  let regtext = '(\\w{' + length + '})';
+  let Reg = new RegExp(regtext);
+  let replaceCount = this.repeatStr(replaceText, length);
+  return str.replace(Reg, replaceCount);
+};
+/**
+ * @description 字符串结束位置加密
+ * @param regIndex 加密长度
+ * @param ARepText 加密的字符 （默认*）
+ */
+const encryptEndStr = (str,length,replaceText = '*') => {
+  return this.encryptStartStr(str.split('').reverse().join(''),length,replaceText).split('').reverse().join('');
+};
+
+//console.log(`加密字符 ${ecDo.encryptStr('18819233362','3,7','+')}`)
+//result:188+++++362
+//console.log(`不加密字符 ${ecDo.encryptUnStr('18819233362','3,7','+')}`)
+//result:+++19233+++
+//console.log(`字符串开始位置加密 ${ecDo.encryptStartStr('18819233362','4')}`)
+//result:****9233362
+//console.log(`字符串结束位置加密 ${ecDo.encryptEndStr('18819233362','4')}`)
+//result:1881923****
+/**
+ * json 转  Form
+ */
+const json2Form = json => {
+  var str = []
+  for(var p in json){
+    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(json[p]))
+  }
+  return str.join("&")
+};
+/**
+ * 写cookie
+ * @param {Object} name
+ * @param {Object} value
+ * @param {Object} hours
+ */
+function _writeCookie(name, value, hours) {
+  var expire = "";
+  if (hours != null) {
+    expire = new Date((new Date()).getTime() + hours * 3600000);
+    expire = "; expires=" + expire.toGMTString();
+  }
+  document.cookie = name + "=" + escape(value) + expire + ";path=/;domain=.yesmywine.com";
+}
+/**
+ * 设置domain
+ */
+   function setDomain() {
+     var d = document.domain;
+     if (d.indexOf('.')<0 || isIP(d)) return;
+     var k = d.split('.'), d1=k[k.length-1], d2=k[k.length-2], d3=k[k.length-3];
+     document.domain = (d2=='com' || d2=='net')?(d3+'.'+d2+'.'+d1):(d2+'.'+d1);
+   }
+   /**
+    * ip正则
+    * @param {Object} str
+    */
+function isIP(str) {
+  var re = /^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$/;
+  return re.test(str);
+}
+/**
+ * 获取domain
+ * @param {Object} url
+ */
+function getDomain(url) {
+  if (url.indexOf('http://')==0) return url.split('/')[2];
+  if (url.indexOf('https://')==0) return url.split('/')[2];
+  return '';
+}
 /**
  * 模块抛出
  */
